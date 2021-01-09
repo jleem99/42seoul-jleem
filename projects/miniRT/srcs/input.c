fcb636435e6cb05a6cc3419ed7ef754d624794d9
jleem <jleem@students.42seoul.kr>
2021-01-10 03:16:30 +0900

    miniRT: Use keybinds as array, Add input polling (is_keydown)

--- projects/miniRT/srcs/input.c
+++ projects/miniRT/srcs/input.c
@@ -3,51 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   input.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jleem <jleem@students.42seoul.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 17:34:32 by jleem             #+#    #+#             */
-/*   Updated: 2021/01/09 09:55:34 by jleem            ###   ########.fr       */
+/*   Updated: 2021/01/09 18:50:28 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "input.h"
+#include "libft_bonus.h"
 #include <X11/X.h>
 #include <mlx.h>
 #include <stdlib.h>
-
-#define NULL (void *)0
 
 // a key is pressed, it will print the key code in the terminal.
 // the mouse if moved, it will print the current position of that mouse in the terminal.
 // a mouse is pressed, it will print the angle at which it moved over the window to the terminal.
 
-t_keybind *g_keybinds;
+t_array			*g_keybinds;
 
 static int		input_manager(int keycode, int iskeydown, void (*fp)())
 {
-	static int			keybind_num;
-	static t_keybind	*g_keybinds;
-	t_keybind			*keybinds_new;
-	size_t				i;
+	t_keybind	*keybind;
+	size_t		i;
 
-	i = 0;
+	if (!(keybind = malloc(sizeof(*keybind))))
+		return (0);
 	if (fp)
 	{
-		if (!(keybinds_new = malloc(sizeof(*g_keybinds) * (keybind_num + 1))))
-			return (0);
-		while (i < keybind_num)
-			keybinds_new[i] = g_keybinds[i];
-		keybinds_new[i].keycode = keycode;
-		keybinds_new[i].fp = fp;
-		free(g_keybinds);
-		g_keybinds = keybinds_new;
-		keybind_num++;
+		keybind->keycode = keycode;
+		keybind->iskeydown = iskeydown;
+		keybind->fp = fp;
+		ft_array_push(g_keybinds, keybind);
 	}
 	else
-		while (i < keybind_num)
-			if (g_keybinds[i].keycode == keycode)
-				fp();
-	
+	{
+		i = 0;
+		while (i < g_keybinds->size)
+			if (((t_keybind *)(g_keybinds->data[i]))->keycode == keycode)
+				((t_keybind *)(g_keybinds->data[i]))->fp();
+	}
 	printf("[%d]keycode:%d|\n", iskeydown, keycode);
 	return (1);
 }
@@ -70,6 +65,8 @@ void			init_input(void *mlx, void *win)
 
 	param.mlx = mlx;
 	param.win = win;
+	if (!g_keybinds)
+		g_keybinds = ft_make_array(0);
 	mlx_hook(win, KeyPress, KeyPressMask, input_hook_press, &param);
 	mlx_hook(win, KeyRelease, KeyReleaseMask, input_hook_release, &param);
 	mlx_do_key_autorepeatoff(mlx);
@@ -82,5 +79,11 @@ void			bind_input(int keycode, int keydown, void (*fp)())
 
 int				is_keydown(int keycode)
 {
-	
+	size_t		i;
+
+	i = 0;
+	while (i < g_keybinds->size)
+		if (((t_keybind *)(g_keybinds->data[i]))->keycode == keycode)
+			return (((t_keybind *)(g_keybinds->data[i]))->iskeydown);
+	return (0);
 }
